ef9c64840714770f207e4d86f0d1b8772110028e
Carlos D <dario20049@gmail.com>
2024-12-17 18:52:02 +0900

    Initial commit

---
+++ src/main/java/dev/dario/gastrotrackapi/GastrotrackApiApplication.java
@@ -0,0 +1,13 @@
+package dev.dario.gastrotrackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastrotrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastrotrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dario/gastrotrackapi/GastrotrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dario.gastrotrackapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastrotrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
